72ab488fd1999599f6cdf5a6e7a8b8c5a6c9e0f9
radheyent <vi.radheyenterprises@gmail.com>
2025-10-22 19:23:07 +0530

    Change SCRIPT_URL in config.js
    Updated SCRIPT_URL in the configuration.

--- config.js
+++ config.js
@@ -2,5 +2,6 @@
 const CONFIG = {
     SHEET_ID: '1PHEUJCf7jd2gJ1_ViLBkjrIW5-50gsJ5HBUr6BDkehQ',
     API_KEY: 'AIzaSyB6jzspcrkP2xqHjCJrTQXFTjLOQ9igVuA',
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzEt_MhIPLS42td0hFvYlZUboJm7jq6zoCNXueojuGzW2c41gTOqS9g8S5Yj-_ox7f_/exec'
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyEbbOrzL49byfjYd2SpDPiwEt2Gdzr8SLJ6Ft3VvMupBuQiQ-xmtmlAwIzp56GFI9B/exec'
 };
+
