1c0fef558770b71422f17e9d2e5e13f09953e17c
radheyent <vi.radheyenterprises@gmail.com>
2025-10-23 18:50:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const CONFIG = {
     SHEET_ID: '1PHEUJCf7jd2gJ1_ViLBkjrIW5-50gsJ5HBUr6BDkehQ',
     API_KEY: 'AIzaSyB6jzspcrkP2xqHjCJrTQXFTjLOQ9igVuA',
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzRgY2VlpUyOp5h0Bbxp2zSrywhjyWdoA_MBBYo-AslFEmB8sQmYnAt_hmqYRevFuXB/exec'
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyYbaBxMcWpfuLrWLL07i3Zt-W3PNZfEC70sMi9I0xd885zEbTZFATwqlSwM6wobebW/exec'
 };
 
 console.log('✅ Config loaded');
+
